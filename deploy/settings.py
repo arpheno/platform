593018ae09a82f1afbbd2456f964e01d74b3cee6
Sebastian Wozny <arpheno@gmail.com>
2013-12-22 23:03:45 +0100

    Again updated deployment/settings.py

--- deploy/settings.py
+++ deploy/settings.py
@@ -114,10 +114,8 @@ ROOT_URLCONF = 'platform.urls'
 WSGI_APPLICATION = 'platform.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+        PROJECT_ROOT + '/templates/',
+        )
 
 INSTALLED_APPS = (
     'django.contrib.auth',

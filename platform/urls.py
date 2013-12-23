4f416d97100d19e272101f2d0470cd472dbcf889
Sebastian Wozny <arpheno@gmail.com>
2013-12-23 02:54:49 +0100

    Contact Button works now

--- platform/urls.py
+++ platform/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 from views import index, header, login, logout, manage_account
+from views import contact
 
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^logout/', logout, name='logout'),
     url(r'^account/', manage_account, name='account'),
     url(r'^news/', include('news.urls'), name='news'),
+    url(r'^contact/', contact, name='contact'),
 )
 
 if settings.DEBUG:
--- platform/views.py
+++ platform/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 def index(request):
     return render(request, 'platform/index.html')
+def contact(request):
+    return render(request, 'platform/contact.html')
 def header(request):
     return render(request, 'platform/header.html')
 def logout(request):

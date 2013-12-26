5d00b6d401c576b934dbd0054690ad7bec524882
Sebastian Wozny <arpheno@gmail.com>
2013-12-26 17:14:30 +0100

    Added async.models.lastmodified.

---
+++ async/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class LastModified(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.name
--- history/models.py
+++ history/models.py
@@ -1,5 +1,6 @@
-import datetime
 from django.db import models
+from async.models import LastModified
+
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
@@ -7,12 +8,17 @@ class Event(models.Model):
         ('training', 'Training'),
         ('operational', 'Operational'),
     )
-    type = models.CharField(max_length=11,
-                                      choices=EVENT_TYPE_CHOICES,
-                                      default='training')
+    type = models.CharField(
+        max_length=11,
+        choices=EVENT_TYPE_CHOICES,
+        default='training')
     date = models.DateTimeField()
     description = models.TextField()
     trainers = models.TextField()
+
+    def save(self, *args, **kwargs):
+        LastModified.objects.get(name="Event").save()
+        super(Event, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return self.name
-# Create your models here.

884c45895ab058b5ac012ffb8bbeb3d1548383ea
Sebastian Wozny <arpheno@gmail.com>
2013-12-27 03:57:33 +0100

    Trainings and Events work by ajax only now.

--- async/views.py
+++ async/views.py
@@ -5,13 +5,12 @@ from models import LastModified
 from email import utils
 import time
 
-
 def history(request):
     events = Event.objects.order_by('date')
     data = serializers.serialize("json", events)
-    lm = LastModified.objects.get(name="Event")
-    lm = utils.formatdate(time.mktime(lm.date.timetuple()))
+    la = LastModified.objects.get(name="Event")
+    lm = str(la.date)
     result = HttpResponse(data, content_type="application/json")
-    result['Last-Modified'] =  lm
+    result['Last-Modified'] = lm
     return result
 # Create your views here.

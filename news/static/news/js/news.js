27b7f4602a9c19d50679af986b67ecf4dfd17b11
Sebastian Wozny <arpheno@gmail.com>
2013-12-29 23:34:57 +0100

    Fixed offset on headline in news.

--- news/static/news/js/news.js
+++ news/static/news/js/news.js
@@ -1,6 +1,6 @@
 function NEntry(entry) {
     var base=$("<div></div>").addClass("post");
-    var headline=$("<h2></h2>").html('<a href="#">'+entry.headline+'</a>');
+    var headline=$("<h2></h2>").addClass("post_title").html('<a href="#">'+entry.headline+'</a>');
     var pub=$("<ul></ul>").html('<li>'+entry.pub_date+'</li>');
     var content=$("<div></div>").addClass("post_content").html(entry.message);
     var meta=$("<div></div>").addClass("post_meta").html(' <div class="post_meta"> <ul class="clearfix"> <li class="post_category"> Published in <a href="#" title="View all posts in News" rel="category tag">News</a>, <a href="#" title="View all posts in TrainingTeam" rel="category tag">Training Team</a> </li> </ul> </div>');

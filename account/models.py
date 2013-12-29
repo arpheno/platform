c6f208d0636105d18278044d9d92a4c43ea88902
Sebastian Wozny <arpheno@gmail.com>
2013-12-30 00:13:06 +0100

    Extended user model

--- account/models.py
+++ account/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 class TrtUser(AbstractUser):
     registration = models.CharField(max_length=100)
+    lc = models.CharField(max_length=50)
+    trainings_delivered = models.IntegerField()
+    profile_picture = models.ImageField(upload_to="users")
+    join_date = models.DateField()
+    languages = models.TextField(max_length=100)
+    preferred_topics = models.TextField(max_length=100)
+

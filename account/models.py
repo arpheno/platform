5deae993bec4ab4e9e26607f77f1cf26ed8b2897
Sebastian Wozny <arpheno@gmail.com>
2013-12-31 01:05:59 +0100

    Added birthdate and contact info to user model.

--- account/models.py
+++ account/models.py
@@ -7,7 +7,8 @@ class TrtUser(AbstractUser):
     lc = models.CharField(max_length=50)
     trainings_delivered = models.IntegerField()
     profile_picture = models.ImageField(upload_to="users")
+    born_on = models.DateField()
     join_date = models.DateField()
     languages = models.TextField(max_length=100)
     preferred_topics = models.TextField(max_length=100)
-
+    contact = models.TextField(max_length=100)

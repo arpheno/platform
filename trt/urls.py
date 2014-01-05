0c8b5e62a04441f14db9255af1179a8bb1be5fcd
Sebastian Wozny <arpheno@gmail.com>
2014-01-05 13:42:13 +0100

    Absolute url works for pool now

--- trt/urls.py
+++ trt/urls.py
@@ -1,20 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from account.models import TrtUser as User
 admin.autodiscover()
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView
 
 
 view = TemplateView.as_view(template_name='trt/index.html')
 urlpatterns = patterns(
     '',
-    url(r'^$',view,{"target": "news"}),
-    url(r'^news/$',view,{"target": "news"} ),
-    url(r'^training/$',view,{"target": "training"} ),
+    url(r'^$', view, {"target": "news"}),
+    url(r'^news/$', view, {"target": "news"}),
+    url(r'^training/$', view, {"target": "training"}),
     url(r'^contact/$', view, {"target": "contact"}),
-    url(r'^operational/$', view,{"target": "operational"}),
+    url(r'^operational/$', view, {"target": "operational"}),
     url(r'^admin/', include(admin.site.urls), name='admin'),
-    url(r'^pool/', ListView.as_view(model=User)),
+    url(r'^pool/$', view, {"target": "pool"}),
     url(r'^account/', include('account.urls')),
     url(r'^async/', include('async.urls')),
     url(r'^materials/', include('materials.urls'), name='materials'),
